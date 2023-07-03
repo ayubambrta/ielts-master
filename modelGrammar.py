11562bae95492228e8bf382cb10ea51f9ee39aaf
Ayub Ambarita <01ayubambarita@gmail.com>
2023-07-03 13:15:34 +0700

    Update Grammar v1.0

--- modelGrammar.py
+++ modelGrammar.py
@@ -96,7 +96,7 @@ def to_level(band_eval):
                     else:
                         band = 9
 
-    return band, band_eval.asr_text
+    return band
     
 # Define function to find root
 def find_root_of_sentence(doc):
