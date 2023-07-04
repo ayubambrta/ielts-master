1bff46beb7735733ac6f59e3a508f2e0bd374aa9
Ayub Ambarita <01ayubambarita@gmail.com>
2023-07-04 16:16:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ def upload():
 
 # Deploy debug must delete
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
